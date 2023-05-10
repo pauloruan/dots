b328af73fe55c20271e08fede203a621990c638e
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-05-10 17:27:54 -0300

    refactor: Font change.

--- setup/install-fonts.sh
+++ setup/install-fonts.sh
@@ -2,7 +2,7 @@
 # Script de instalação de fontes
 
 # Verificando se a fonte já está instalada
-if fc-list | grep -q "JetBrains Mono"; then
+if fc-list | grep -q "JetBrainsMonoNerdFont"; then
   echo -e "\033[1m\033[31mFonte JetBrains Mono já instalada. Saindo.\033[0m\n"
   exit 0
 fi
@@ -15,23 +15,23 @@ fi
 
 # Baixando fontes com HTTPS
 echo "\033[1m\033[36mBaixando fontes com HTTPS...\033[0m\n"
-wget -O ~/.fonts/JetBrainsMono-2.304.zip https://download.jetbrains.com/fonts/JetBrainsMono-2.304.zip # JetBrains Mono font
+wget -O ~/.fonts/JetBrainsMono.zip https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.0/JetBrainsMono.zip # JetBrainsMono Nerd Font
 
 # Descompactando fontes
 echo "\033[1m\033[36mDescompactando fontes...\033[0m\n"
-unzip ~/.fonts/JetBrainsMono-2.304.zip -d ~/.fonts
+unzip ~/.fonts/JetBrainsMono.zip -d ~/.fonts
 
 # Removendo arquivos desnecessários
 echo "\033[1m\033[36mRemovendo arquivos desnecessários...\033[0m\n"
-rm ~/.fonts/JetBrainsMono-2.304.zip
+rm ~/.fonts/JetBrainsMono.zip
 
 # Atualizando cache de fontes
 echo "\033[1m\033[36mAtualizando cache de fontes...\033[0m\n"
-fc-cache -f -v ~/.fonts/fonts/ttf
+fc-cache -f -v ~/.fonts/*.ttf
 
 # Listando fontes instaladas
 echo "\033[1m\033[36mListando fontes instaladas...\033[0m\n"
-fc-list | grep JetBrains
+fc-list | grep JetBrainsMonoNerdFont
 
 # Mensagem de conclusão
 echo "\033[1m\033[32mInstalação das fontes concluída.\033[0m\n"
