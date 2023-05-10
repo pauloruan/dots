9068d186afa6a718c7db0eddf365c5c55ab87c09
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2023-05-10 10:46:44 -0300

    refactor: Message styling.

--- setup/install-fonts.sh
+++ setup/install-fonts.sh
@@ -3,32 +3,35 @@
 
 # Verificando se a fonte já está instalada
 if fc-list | grep -q "JetBrains Mono"; then
-  echo "Fonte JetBrains Mono já instalada. Saindo."
+  echo -e "\033[1m\033[31mFonte JetBrains Mono já instalada. Saindo.\033[0m\n"
   exit 0
 fi
 
 # Verificando se o diretório ~/.fonts existe
 if [ ! -d ~/.fonts ]; then
-  echo "Diretório ~/.fonts não existe. Criando..."
+  echo "\033[1m\033[36mDiretório ~/.fonts não existe. Criando...\033[0m\n"
   mkdir ~/.fonts
 fi
 
 # Baixando fontes com HTTPS
-echo "Baixando fontes com HTTPS..."
-curl -o ~/.fonts/JetBrainsMono-2.304.zip https://download.jetbrains.com/fonts/JetBrainsMono-2.304.zip # JetBrains Mono font
+echo "\033[1m\033[36mBaixando fontes com HTTPS...\033[0m\n"
+wget -O ~/.fonts/JetBrainsMono-2.304.zip https://download.jetbrains.com/fonts/JetBrainsMono-2.304.zip # JetBrains Mono font
 
 # Descompactando fontes
-echo "Descompactando fontes..."
+echo "\033[1m\033[36mDescompactando fontes...\033[0m\n"
 unzip ~/.fonts/JetBrainsMono-2.304.zip -d ~/.fonts
 
 # Removendo arquivos desnecessários
-echo "Removendo arquivos desnecessários..."
+echo "\033[1m\033[36mRemovendo arquivos desnecessários...\033[0m\n"
 rm ~/.fonts/JetBrainsMono-2.304.zip
 
 # Atualizando cache de fontes
-echo "Atualizando cache de fontes..."
-fc-cache -f -v ~/.fonts
+echo "\033[1m\033[36mAtualizando cache de fontes...\033[0m\n"
+fc-cache -f -v ~/.fonts/fonts/ttf
 
 # Listando fontes instaladas
-echo "Listando fontes instaladas..."
+echo "\033[1m\033[36mListando fontes instaladas...\033[0m\n"
 fc-list | grep JetBrains
+
+# Mensagem de conclusão
+echo "\033[1m\033[32mInstalação das fontes concluída.\033[0m\n"
